afd7d3957683e51c6610123f26e0a25712574f7d
EazePark <143898886+EazePark@users.noreply.github.com>
2024-01-11 20:19:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-import smtplib
+import yagmail
 
 app = Flask(__name__)
 
@@ -7,61 +7,48 @@ app = Flask(__name__)
 payments = {}
 
 # Gmail SMTP configuration (Replace with your Gmail credentials)
-smtp_server = "smtp.gmail.com"
-smtp_port = 587
 smtp_username = "test1ofpython@gmail.com"
 smtp_password = "hwkwrrdtgcmolelx"
 
-
 def send_email(car_number, money):
-  subject = "Parking Payment Receipt"
-  body = f"Car Number: {car_number}\nMoney Paid: {money}"
+    subject = "Parking Payment Receipt"
+    body = f"Car Number: {car_number}\nMoney Paid: {money}"
 
-  try:
-    # Create an SMTP connection
-    server = smtplib.SMTP(smtp_server, smtp_port)
-    server.starttls()
-    server.login(smtp_username, smtp_password)
+    try:
+        # Create a yagmail SMTP connection
+        yag = yagmail.SMTP(smtp_username, smtp_password)
 
-    # Send the email
-    server.sendmail(smtp_username, "ineazepark@gmail.com",
-                    f"Subject: {subject}\n\n{body}")
+        # Send the email
+        yag.send("ineazepark@gmail.com", subject, body)
 
-    # Quit the SMTP server
-    server.quit()
-
-    print("Email sent successfully")
-  except Exception as e:
-    print(f"Email sending failed: {str(e)}")
-
+        print("Email sent successfully")
+    except Exception as e:
+        print(f"Email sending failed: {str(e)}")
 
 @app.route("/pay", methods=["GET", "POST"])
 def pay():
-  if request.method == "POST":
-    car_number = request.form.get("car_number")
-    money = request.form.get("money")
+    if request.method == "POST":
+        car_number = request.form.get("car_number")
+        money = request.form.get("money")
 
-    # Store car_number and money in the dictionary
-    payments[car_number] = money
+        # Store car_number and money in the dictionary
+        payments[car_number] = money
 
-    # Send an email
-    send_email(car_number, money)
+        # Send an email using yagmail
+        send_email(car_number, money)
 
-    # Redirect to payment_successful.html on successful payment
-    return redirect(url_for("payment_successful"))
+        # Redirect to payment_successful.html on successful payment
+        return redirect(url_for("payment_successful"))
 
-  return render_template("index.html", payments=payments)
-
+    return render_template("index.html", payments=payments)
 
 @app.route("/payment_successful")
 def payment_successful():
-  return render_template("payment_successful.html")
-
+    return render_template("payment_successful.html")
 
 @app.route("/")
 def index():
-  return render_template("index.html", payments=payments)
-
+    return render_template("index.html", payments=payments)
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
